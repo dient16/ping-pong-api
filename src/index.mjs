d9a2cf9c7c2fdbd399a81637d21aa8ffad1b0cec
dient16 <dientc16@gmail.com>
2024-09-19 14:27:21 +0700

    feat(app): add request count feature and log with colors

--- src/index.mjs
+++ src/index.mjs
@@ -2,6 +2,8 @@ import express from "express";
 import axios from "axios";
 import pLimit from "p-limit";
 import dotenv from "dotenv";
+import cron from "node-cron";
+import morgan from "morgan";
 
 dotenv.config();
 
@@ -9,31 +11,64 @@ const app = express();
 const PORT = process.env.PORT || 8058;
 
 const apiURLs = process.env.API_URLS ? process.env.API_URLS.split("|") : [];
-
-const callApi = async (apiURL) => {
-  try {
-    const response = await axios.get(apiURL);
-    console.log(`Response from ${apiURL}:`, response.data);
-  } catch (error) {
-    console.error(`Error calling ${apiURL}:`, error.message);
-  }
-};
+const otherServerURL = process.env.OTHER_SERVER_URL;
+const cronInterval = process.env.CRON_INTERVAL || 20;
 
 const limit = pLimit(Number.parseInt(process.env.LIMIT, 10) || 5);
 
-const callApisSimultaneouslyWithLimit = () => {
-  setTimeout(() => {
-    Promise.all(apiURLs.map((apiURL) => limit(() => callApi(apiURL))))
-      .then(() => console.log("All API calls completed"))
-      .catch((error) => console.error("Error in API calls:", error.message));
-  }, 5000);
+app.use(morgan("combined"));
+
+let requestCount = 0;
+
+const callApis = async () => {
+  try {
+    const apiCalls = apiURLs.map((apiURL) =>
+      limit(async () => {
+        try {
+          const response = await axios.get(apiURL, { timeout: 5000 });
+          requestCount++;
+          console.log(`Response from ${apiURL}:`, response.data);
+        } catch (error) {
+          console.error(`Error calling ${apiURL}:`, error.message);
+        }
+      })
+    );
+
+    await Promise.all(apiCalls);
+    console.log("All API calls completed.");
+  } catch (error) {
+    console.error("Error in API calls:", error.message);
+  }
 };
-app.get("/ping", (req, res) => {
-  callApisSimultaneouslyWithLimit();
-  res.send("pong");
+
+const callOtherServerAndApis = async () => {
+  try {
+    const response = await axios.get(otherServerURL);
+    requestCount++;
+    console.log(
+      `Response from other server (${otherServerURL}):`,
+      response.data
+    );
+    await callApis();
+  } catch (error) {
+    console.error(
+      `Error during communication with other server:`,
+      error.message
+    );
+  }
+};
+
+app.get("/ping", async (req, res) => {
+  res.send(`pong - Total requests made: ${requestCount}`);
 });
 
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
-  callApisSimultaneouslyWithLimit();
+
+  cron.schedule(`*/${cronInterval} * * * * *`, async () => {
+    console.log(
+      `Calling other server and APIs every ${cronInterval} seconds...`
+    );
+    await callOtherServerAndApis();
+  });
 });
